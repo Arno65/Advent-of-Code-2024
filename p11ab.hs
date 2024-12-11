3f9cb58b9c443af8af70228eaae020b5c08c88c0
Arno Jacobs <21236973+Arno65@users.noreply.github.com>
2024-12-11 14:47:33 +0100

    Add files via upload

--- p11ab.hs
+++ p11ab.hs
@@ -11,8 +11,8 @@
 
 import Data.List.Split  (splitOn)
 
-type Mem    = ((Int,Int),Int)    -- ((stone,blink),value)
-type Memory = [Mem]
+type Mem    = (Int,Int)     -- (stone,value)
+type Memory = [[Mem]]       -- index !! blink
 
 -- Some initials
 filename :: String
@@ -25,7 +25,6 @@ counts2 = 75
 numbers :: String -> [Int]
 numbers s = map read (splitOn " " s)
 
-
 evenDigits :: Int -> Bool
 evenDigits = even . length . show
 
@@ -36,18 +35,19 @@ splitEven n = ( read nsl, read nsr )
         hnsl        = div (length ns) 2
         (nsl,nsr)   = (take hnsl ns, drop hnsl ns)
 
-fromMemory :: (Int,Int) -> Memory -> (Bool,Int)
-fromMemory sb memory    | hits == []    = (False,0) 
-                        | otherwise     = (True,value)
+fromMemory :: Int -> Int -> Memory -> (Bool,Int)
+fromMemory stone blink memory   | hits == []        = (False,0) 
+                                | otherwise         = (True,snd (head hits))
     where
-        hits        = filter (\(sbm,_) -> sbm == sb) memory
-        (_,value)   = head hits
+        hits = filter (\(ts,_) -> ts==stone) (memory !! blink)
 
 updateMemory :: Int -> Int -> Int -> Memory -> Memory
-updateMemory stone blink value memory   | inMemory  = memory
-                                        | otherwise = memory ++ [((stone,blink),value)]
+updateMemory stone blink value memory   
+    | inMemory  = memory
+    | otherwise = take blink memory ++ [insert] ++ drop (blink+1) memory
     where
-        inMemory = elem (stone,blink) $ map fst memory
+        inMemory    = elem stone $ map fst (memory !! blink)
+        insert      = (memory !! blink) ++ [(stone,value)]
 
 solve :: Int -> Int -> Memory -> (Int,Memory)
 solve _     0     memory    = (1,memory) 
@@ -58,7 +58,7 @@ solve stone blink memory
     | otherwise             = (value2024,  updateMemory s2024 nBlink value2024  m2024) 
         where
             nBlink                  = blink - 1
-            (inMemory,value)        = fromMemory (stone,blink) memory
+            (inMemory,value)        = fromMemory stone blink memory
             (value0,memory0)        = solve 1 nBlink memory
             (leftStone,rightStone)  = splitEven stone
             (vLeft,mLeft)           = solve leftStone  nBlink memory
@@ -70,7 +70,9 @@ solve stone blink memory
 -- (In this Haskell code) 
 -- Starting with a clear memory for every new stone is 1.5 times faster
 countStones :: Int -> [Int] -> Int 
-countStones blinks = sum . map (fst . (\stone -> solve stone blinks []))
+countStones blinks = sum . map (fst . (\stone -> solve stone blinks emptyMemory))
+    where 
+        emptyMemory = replicate (blinks+1) []   -- create a list for every blink
 
 
 main :: IO ()
@@ -83,7 +85,7 @@ main = do   putStrLn "Advent of Code 2024 - day 11 (Haskell)"
             putStr "Part two: number of stones after "
             putStr $ show counts2
             putStr " blinks: "
-            print $ countStones counts2 day11   --- still slow, 3m46s on my M1
+            print $ countStones counts2 day11   
             putStrLn "0K.\n"
 
 
